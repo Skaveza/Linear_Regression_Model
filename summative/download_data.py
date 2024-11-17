b7ec89045ef1b945382f11d8c5035b165f23a8a2
Skaveza <s.mwahoni@alustudent.com>
2024-11-17 23:31:44 +0200

    lrmodel

--- summative/download_data.py
+++
@@ -1,8 +0,0 @@
-import os
-
-"""This module downloads the dataset"""
-
-
-dataset = "alexteboul/heart-disease-health-indicators-dataset"
-os.system(f"kaggle datasets download -d {dataset} --unzip -p ./data")
-print("Dataset downloaded to ./data")
